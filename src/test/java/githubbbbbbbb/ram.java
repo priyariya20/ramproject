da438f90ca9c51b158ec2e718d2878bcb5dd588b
priyariya20 <priyariya.vlr97@gmail.com>
2021-10-20 13:53:29 +0530

    ram's update code

--- src/test/java/githubbbbbbbb/ram.java
+++ src/test/java/githubbbbbbbb/ram.java
@@ -2,6 +2,9 @@ package githubbbbbbbb;
 
 public class ram {
 public static void main(String[] args) {
+	System.out.println("ram");
+	System.out.println("playing");
+	System.out.println("cricket");
 	
 }
 }
